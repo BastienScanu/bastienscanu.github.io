a421dab4bdf4437e8509f573c6220e8968738662
bastien <bastien.scanu@gmail.com>
2016-10-10 15:37:06 +0200

    gulp task pour la génération de favicon

---
+++ README.md
@@ -0,0 +1,2 @@
+## [Personal Website](https://bitbucket.org/Bastien42/dev)"
+> Hello !
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp');
 const HubRegistry = require('gulp-hub');
 const browserSync = require('browser-sync');
-
 const conf = require('./conf/gulp.conf');
 
 // Load some files into the registry
@@ -10,7 +9,7 @@ const hub = new HubRegistry([conf.path.tasks('*.js')]);
 // Tell gulp to use the tasks just loaded
 gulp.registry(hub);
 
-gulp.task('build', gulp.series(gulp.parallel('other', 'webpack:dist')));
+gulp.task('build', gulp.series(gulp.parallel('other', 'webpack:dist', 'generate-favicon', 'inject-favicon-markups')));
 gulp.task('test', gulp.series('karma:single-run'));
 gulp.task('test:auto', gulp.series('karma:auto-run'));
 gulp.task('serve', gulp.series('webpack:watch', 'watch', 'browsersync'));
