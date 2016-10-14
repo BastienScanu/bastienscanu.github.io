b4c380189b0cbe0c74a3638644e773fa7e253478
Bastien Scanu <bastien42@gmail.com>
2016-10-14 14:36:19 +0200

    modification du header, début navra

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,17 +1,26 @@
 import React, {Component} from 'react';
-import logoUrl from '../../images/logo-white.svg';
+import {Logo} from './logo';
 
 export class Header extends Component {
   render() {
     return (
       <header>
-        <p className="container">
-          <img src={logoUrl} width="24" height="24" alt="Bastien Scanu"/>
-          <a href="http://www.bastien-scanu.com" target="_blank">
-            Bastien Scanu
-          </a>
-        </p>
+        <div className="container">
+          <Logo size={48} white/>
+          <ul className="navBar">
+            <li>Accueil</li>
+            <li>Compétences</li>
+            <li>Parcours</li>
+            <li>Contact</li>
+          </ul>
+          <ul className="langs">
+            <li>EN</li>
+            <li>FR</li>
+            <li>ES</li>
+          </ul>
+        </div>
       </header>
+
     );
   }
 }
