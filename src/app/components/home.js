8eeb8391a81fa47b2955abbeab46638b43a3c429
bastien <bastien.scanu@gmail.com>
2016-10-14 14:35:56 +0200

    component home

---
+++ src/app/components/home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <div className="container">
+          <h1>Hello !</h1>
+          <h2>I'm a web developper</h2>
+        </div>
+      </div>
+    );
+  }
+}
--- src/app/components/title.js
+++ src/app/components/title.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export class Title extends Component {
   render() {
     return (
-      <div className="home">
+      <div className="panel">
         <div className="container">
           <h1>'Allo, 'Allo!</h1>
           <h2>Always a pleasure scaffolding your apps.</h2>
