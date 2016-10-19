ad70ebdbc83132cca2421f88b2bbec1e2303ca05
bastien <bastien.scanu@gmail.com>
2016-10-19 23:11:04 +0200

    component About

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -8,8 +8,9 @@ export class Header extends Component {
       <header>
         <div className="container">
           <Logo size={48} white/>
-          <Scrollspy items={['home', 'skills', 'experience', 'contact']} currentClassName="current-menu" className="navBar">
+          <Scrollspy items={['home', 'about', 'skills', 'experience', 'contact']} currentClassName="current-menu" className="navBar">
             <li><a href="#home">Accueil</a></li>
+            <li><a href="#about">About</a></li>
             <li><a href="#skills">Compétences</a></li>
             <li><a href="#experience">Parcours</a></li>
             <li><a href="#contact">Contact</a></li>
