9dabb6547ba37552ab112f2512f7108d460cf7b9
bastien <bastien.scanu@gmail.com>
2016-10-19 23:10:21 +0200

    injectTap dans l'index

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory} from 'react-router';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import {Main} from './app/main';
 import './app/style/main.scss';
 
+injectTapEventPlugin();
 ReactDOM.render(
   <Router history={browserHistory}>
     <Route path="/" component={Main}/>
