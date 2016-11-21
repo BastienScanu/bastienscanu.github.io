2d86c6b872da8515792da0483c23c28cda658331
bastien <bastien.scanu@gmail.com>
2016-11-21 13:47:07 +0100

    debug nouvelle version React

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory} from 'react-router';
-import injectTapEventPlugin from 'react-tap-event-plugin';
+// import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import {green500, green600, green700, red500} from 'material-ui/styles/colors';
@@ -21,7 +21,7 @@ const greenTheme = getMuiTheme({
 import {Main} from './app/main';
 import './app/style/main.scss';
 
-injectTapEventPlugin();
+// injectTapEventPlugin();
 ReactDOM.render(
   <MuiThemeProvider muiTheme={getMuiTheme(greenTheme)}>
     <I18nextProvider i18n={i18n}>
