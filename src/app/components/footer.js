7ba8c9b474a1104d34fe93cc18f673f3aef00a60
Bastien Scanu <bastien42@gmail.com>
2017-01-21 13:46:10 +0100

    Footer + star wars effect

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -1,19 +1,55 @@
-import React, {Component, PropTypes} from 'react';
+import React, {PropTypes} from 'react';
 import {translate} from 'react-i18next';
+import FontAwesome from 'react-fontawesome';
 
-class MyFooter extends Component {
+const MyFooter = React.createClass({
+  getInitialState() {
+    return {open: false};
+  },
+
+  handleOpen() {
+    this.setState({open: true});
+  },
+
+  handleClose() {
+    this.setState({open: false});
+  },
+
   render() {
     const {t} = this.props;
     return (
       <footer>
-        <p>{t('footer:copyright')}</p>
+        <div className="container">
+          <div className="row around-xs">
+            <div>
+              <p><a href="#starWars">{t('footer:about')}</a></p>
+            </div>
+            <div>
+              <p>{t('footer:copyright')}</p>
+            </div>
+            <div>
+              <p className="icons">
+                <a href="https://fr.linkedin.com/in/bastienscanu" target="_blank" title={t('footer:linkedin')}><FontAwesome name="linkedin-square"/></a>
+                <a href="https://github.com/BastienScanu" target="_blank" title={t('footer:github')}><FontAwesome name="github"/></a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div id="starWars">
+          <a href="#close" className="button"></a>
+          <div id="titles">
+            <div id="titlecontent">
+              <p>{t('footer:credits')}</p>
+            </div>
+          </div>
+        </div>
       </footer>
     );
-  }
-}
+  },
 
-MyFooter.propTypes = {
-  t: PropTypes.func
-};
+  propTypes: {
+    t: PropTypes.func
+  }
+});
 
 export default translate(["common", "footer"], {wait: true})(MyFooter);
