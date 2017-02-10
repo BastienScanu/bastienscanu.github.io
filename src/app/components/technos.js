bb94171b26745c4958dd770188262c0fc31d5f7c
Bastien Scanu <bastien42@gmail.com>
2017-02-10 02:03:00 +0100

    Ajout sections Technos + background skills

---
+++ src/app/components/technos.js
@@ -0,0 +1,95 @@
+import React, {Component, PropTypes} from 'react';
+import {translate} from 'react-i18next';
+import Techno from './techno';
+
+class Technos extends Component {
+  render() {
+    const {t} = this.props;
+    const technos = [
+      {
+        key: "angular",
+        avatar: "A"
+      },
+      {
+        key: "react",
+        avatar: "R"
+      },
+      {
+        key: "nodejs",
+        avatar: "N"
+      },
+      {
+        key: "mongo",
+        avatar: "M"
+      },
+      {
+        key: "express",
+        avatar: "E"
+      },
+      {
+        key: "es6",
+        avatar: "ES"
+      },
+      {
+        key: "docker",
+        avatar: "D"
+      },
+      {
+        key: "html",
+        avatar: "H"
+      },
+      {
+        key: "css",
+        avatar: "CSS"
+      },
+      {
+        key: "php",
+        avatar: "P"
+      },
+      {
+        key: "sql",
+        avatar: "S"
+      },
+      {
+        key: "java",
+        avatar: "J"
+      },
+      {
+        key: "c",
+        avatar: "C"
+      },
+      {
+        key: "git",
+        avatar: "G"
+      },
+      {
+        key: "scrum",
+        avatar: "S"
+      },
+      {
+        key: "machine",
+        avatar: "ML"
+      },
+      {
+        key: "socket",
+        avatar: "S"
+      }
+    ];
+    return (
+      <section id="technos">
+        <h3>{t('technos:title')}</h3>
+        <div id="technosList">
+          {technos.map(tech => {
+            return <Techno avatar={tech.avatar} key={tech.key} text={tech.key}/>;
+          })}
+        </div>
+      </section>
+    );
+  }
+}
+
+Technos.propTypes = {
+  t: PropTypes.func
+};
+
+export default translate(["common", "technos"], {wait: true})(Technos);
--- src/app/main.js
+++ src/app/main.js
@@ -5,6 +5,7 @@ import MyFooter from './components/footer';
 import Experience from './components/experience';
 import Contact from './components/contact';
 import Skills from './components/skills';
+import Technos from './components/technos';
 import About from './components/about';
 import ActionButton from './components/actionButton';
 
@@ -16,6 +17,7 @@ export class Main extends Component {
         <Home/>
         <About/>
         <Skills/>
+        <Technos/>
         <Experience/>
         <Contact/>
         <ActionButton/>
