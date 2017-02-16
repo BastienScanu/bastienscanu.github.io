ab768c3c48e421eab02add778d1d49c01bb53e0b
Bastien Scanu <bastien42@gmail.com>
2017-02-16 22:54:08 +0100

    Section ‘Built with’

---
+++ src/app/components/builtWith.js
@@ -0,0 +1,35 @@
+import React, {Component, PropTypes} from 'react';
+import FontAwesome from 'react-fontawesome';
+import {translate} from 'react-i18next';
+
+export class BuiltWith extends Component {
+  render() {
+    const {t} = this.props;
+    const technos = ['nodejs', 'react', 'webpack', 'css', 'sass', 'gulp', 'html', 'es6', 'babel'];
+    return (
+      <section id="builtWith">
+        <div className="container">
+          <h3>{t('builtWith:title')}</h3>
+          <div id="usedTechnos">
+            {technos.map(tech => {
+              return (
+                <div key={tech}>
+                  <img src={`../../images/techs/${tech}.svg`} alt={t(`builtWith:${tech}`)} height="70px"/>
+                </div>
+              );
+            })}
+          </div>
+          <div className="row">
+            <button><FontAwesome name="github"/> {t('builtWith:github')}</button>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+BuiltWith.propTypes = {
+  t: PropTypes.func
+};
+
+export default translate(["common", "builtWith"], {wait: true})(BuiltWith);
--- src/app/main.js
+++ src/app/main.js
@@ -6,6 +6,7 @@ import Experience from './components/experience';
 import Contact from './components/contact';
 import Skills from './components/skills';
 import Technos from './components/technos';
+import BuiltWith from './components/builtWith';
 import About from './components/about';
 import ActionButton from './components/actionButton';
 
@@ -19,6 +20,7 @@ export class Main extends Component {
         <Skills/>
         <Technos/>
         <Experience/>
+        <BuiltWith/>
         <Contact/>
         <ActionButton/>
         <MyFooter/>
