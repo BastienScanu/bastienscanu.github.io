45885df0ad94673922066daa2231587e3df80f70
Bastien Scanu <bastien42@gmail.com>
2017-02-28 13:31:01 +0100

    Test section technos

--- src/app/components/technos.js
+++ src/app/components/technos.js
@@ -1,88 +1,34 @@
 import React, {Component, PropTypes} from 'react';
 import {translate} from 'react-i18next';
-import Techno from './techno';
+import Vivus from 'vivus';
 
 class Technos extends Component {
+  componentDidMount() {
+    return new Vivus(
+      'laptop',
+      {
+        duration: 80,
+        type: "sync",
+        reverseStack: true,
+        animTimingFunction: Vivus.EASE
+      },
+      () => {
+        document.getElementById('side').className += " laptopPng";
+      }
+    );
+  }
   render() {
     const {t} = this.props;
-    const technos = [
-      {
-        key: "angular",
-        avatar: "A"
-      },
-      {
-        key: "react",
-        avatar: "R"
-      },
-      {
-        key: "nodejs",
-        avatar: "N"
-      },
-      {
-        key: "mongo",
-        avatar: "M"
-      },
-      {
-        key: "express",
-        avatar: "E"
-      },
-      {
-        key: "es6",
-        avatar: "ES"
-      },
-      {
-        key: "docker",
-        avatar: "D"
-      },
-      {
-        key: "html",
-        avatar: "H"
-      },
-      {
-        key: "css",
-        avatar: "CSS"
-      },
-      {
-        key: "php",
-        avatar: "P"
-      },
-      {
-        key: "sql",
-        avatar: "S"
-      },
-      {
-        key: "java",
-        avatar: "J"
-      },
-      {
-        key: "c",
-        avatar: "C"
-      },
-      {
-        key: "git",
-        avatar: "G"
-      },
-      {
-        key: "scrum",
-        avatar: "S"
-      },
-      {
-        key: "machine",
-        avatar: "ML"
-      },
-      {
-        key: "socket",
-        avatar: "S"
-      }
-    ];
     return (
       <section id="technos">
         <div className="container">
-          <h3>{t('technos:title')}</h3>
-          <div id="technosList">
-            {technos.map(tech => {
-              return <Techno avatar={tech.avatar} key={tech.key} text={tech.key}/>;
-            })}
+          <div className="row">
+            <div className="col-lg-8 col-md-8 col-sm-8">
+              <h3>{t('technos:title')}</h3>
+            </div>
+            <div id="side" className="col-lg-4 col-md-4 col-sm-4">
+              <object id="laptop" type="image/svg+xml" data="../../images/layout/laptop.svg"></object>
+            </div>
           </div>
         </div>
       </section>
