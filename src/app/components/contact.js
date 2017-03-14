02f010f04da2b216b7f6dd0090f390df6a512815
Bastien Scanu <bastien42@gmail.com>
2017-03-14 17:37:35 +0100

    section contacts responsive

--- src/app/components/contact.js
+++ src/app/components/contact.js
@@ -10,7 +10,7 @@ class Contact extends Component {
     return (
       <section id="contact">
         <div className="container row">
-          <div id="smartphone" className="col-lg-3 col-md-3">
+          <div id="smartphone" className="col-xs-12 col-sm-12 col-lg-3 col-md-3">
             <div id="smartphoneScreen">
               <div className="row">
                 <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
@@ -38,10 +38,12 @@ class Contact extends Component {
               </div>
               <div className="row">
                 <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                  <FontIcon className="material-icons" color={green500} hoverColor={green300}>place</FontIcon>
+                  <a href="https://www.google.fr/maps/place/Lyon/" target="_blank">
+                    <FontIcon className="material-icons" color={green500} hoverColor={green300}>place</FontIcon>
+                  </a>
                 </div>
                 <div className="col-xs-9 col-sm-9 col-md-9 col-lg-9">
-                  <h5>Lyon, France</h5>
+                  <h5><a href="https://www.google.fr/maps/place/Lyon/" target="_blank">Lyon, France</a></h5>
                   <h6>{t('contact:home')}</h6>
                   <Divider/>
                 </div>
