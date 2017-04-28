00ab2f24bfdb57733d5b8985aa2932bb2d001834
Bastien Scanu <bastien42@gmail.com>
2017-04-28 16:14:33 +0200

    Animation de l’action button

--- src/app/components/actionButton.js
+++ src/app/components/actionButton.js
@@ -1,39 +1,74 @@
-import React, {Component, PropTypes} from 'react';
-import FloatingActionButton from 'material-ui/FloatingActionButton';
+import React, {PropTypes} from 'react';
 import ContentAdd from 'material-ui/svg-icons/content/add';
 import {translate} from 'react-i18next';
 import FontIcon from 'material-ui/FontIcon';
 import FontAwesome from 'react-fontawesome';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import {spring, Motion} from 'react-motion';
 
-class ActionButton extends Component {
+const ActionButton = React.createClass({
+  getInitialState() {
+    return {isOpen: false};
+  },
+  initialPosition(childIndex) {
+    return {
+      bottom: spring(-55 - 40 * childIndex)
+    };
+  },
+  handleMouseOver() {
+    this.setState({
+      isOpen: true
+    });
+  },
+  handleMouseOut() {
+    this.setState({
+      isOpen: false
+    });
+  },
   render() {
     const {t} = this.props;
+    const {isOpen} = this.state;
+    const finalPosition = {
+      bottom: spring(0)
+    };
     return (
-      <div id="actionButton">
-        <div className="childButton">
-          <label className="tooltip">{t('common:button.linkedin')}</label>
-          <a href="https://fr.linkedin.com/in/bastienscanu" target="_blank">
-            <FloatingActionButton mini backgroundColor="#0277BD">
-              <FontAwesome name="linkedin-square"/>
-            </FloatingActionButton>
-          </a>
-        </div>
-        <div className="childButton">
-          <label className="tooltip">{t('common:button.mail')}</label>
-          <a href="http://bastien-scanu.com/CV.pdf" target="_blank">
-            <FloatingActionButton mini backgroundColor="#E62A10">
-              <FontIcon className="material-icons">mail</FontIcon>
-            </FloatingActionButton>
-          </a>
-        </div>
-        <div className="childButton">
-          <label className="tooltip">{t('common:button.resume')}</label>
-          <a href="mailto:bastien-scanu@gmail.com" target="_blank">
-            <FloatingActionButton mini backgroundColor="#FF9800">
-              <FontIcon className="material-icons">description</FontIcon>
-            </FloatingActionButton>
-          </a>
-        </div>
+      <div id="actionButton" onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut}>
+        <Motion style={isOpen ? finalPosition : this.initialPosition(2)}>
+          {({bottom}) =>
+            <div className="buttonLabel" style={{bottom}}>
+              <label className="tooltip">{t('common:button.linkedin')}</label>
+              <div className="smallButton blue">
+                <a href="https://fr.linkedin.com/in/bastienscanu" target="_blank">
+                  <FontAwesome name="linkedin-square"/>
+                </a>
+              </div>
+            </div>
+          }
+        </Motion>
+        <Motion style={isOpen ? finalPosition : this.initialPosition(1)}>
+          {({bottom}) =>
+            <div className="buttonLabel" style={{bottom}}>
+              <label className="tooltip">{t('common:button.mail')}</label>
+              <div className="smallButton orange">
+                <a href="http://bastien-scanu.com/CV.pdf" target="_blank">
+                  <FontIcon color="white" className="material-icons">mail</FontIcon>
+                </a>
+              </div>
+            </div>
+          }
+        </Motion>
+        <Motion style={isOpen ? finalPosition : this.initialPosition(0)}>
+          {({bottom}) =>
+            <div className="buttonLabel" style={{bottom}}>
+              <label className="tooltip">{t('common:button.resume')}</label>
+              <div className="smallButton red">
+                <a href="mailto:bastien-scanu@gmail.com" target="_blank">
+                  <FontIcon color="white" className="material-icons">description</FontIcon>
+                </a>
+              </div>
+            </div>
+          }
+        </Motion>
         <div className="mainButton">
           <FloatingActionButton>
             <ContentAdd/>
@@ -41,11 +76,10 @@ class ActionButton extends Component {
         </div>
       </div>
     );
+  },
+  propTypes: {
+    t: PropTypes.func
   }
-}
-
-ActionButton.propTypes = {
-  t: PropTypes.func
-};
+});
 
 export default translate(["common"], {wait: true})(ActionButton);
