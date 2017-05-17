11aca72dda31e7c8fb19bff30c99c99ab046cb1c
Bastien Scanu <bastien42@gmail.com>
2017-05-17 11:31:20 +0200

    ajouts de photos pour les langages et les soft skills

--- src/app/components/skills.js
+++ src/app/components/skills.js
@@ -13,12 +13,23 @@ class Skills extends Component {
           <MainSkills/>
           <div className="row">
             <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-              <div className="stat">
+              <div>
                 <h4>{t('skills:soft')}</h4>
+                <div className="otherSkill">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </p>
+                </div>
               </div>
             </div>
+            <div id="softPic" className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            </div>
+            <div id="languagesPic" className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            </div>
             <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-              <Languages/>
+              <div className="otherSkill">
+                <Languages/>
+              </div>
             </div>
           </div>
         </div>
