52fd82ad6e9487199256d104ade7488bbf0dc169
Bastien Scanu <bastien42@gmail.com>
2017-05-18 13:29:35 +0200

    tag cloud pour les soft skills

--- src/app/components/skills.js
+++ src/app/components/skills.js
@@ -2,6 +2,7 @@ import React, {Component, PropTypes} from 'react';
 import {translate} from 'react-i18next';
 import MainSkills from './mainSkills';
 import Languages from './languages';
+import Soft from './soft';
 
 class Skills extends Component {
   render() {
@@ -12,22 +13,17 @@ class Skills extends Component {
         <div className="container">
           <MainSkills/>
           <div className="row">
-            <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-              <div>
-                <h4>{t('skills:soft')}</h4>
-                <div className="otherSkill">
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                  </p>
-                </div>
+            <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 otherSkill">
+              <div className="skillCard">
+                <Soft/>
               </div>
             </div>
             <div id="softPic" className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
             </div>
             <div id="languagesPic" className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
             </div>
-            <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-              <div className="otherSkill">
+            <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 otherSkill">
+              <div className="skillCard">
                 <Languages/>
               </div>
             </div>
---
+++ src/app/components/soft.js
@@ -0,0 +1,63 @@
+import React, {Component, PropTypes} from 'react';
+import {translate} from 'react-i18next';
+import {TagCloud} from 'react-tagcloud';
+
+class Soft extends Component {
+  render() {
+    const {t} = this.props;
+    const tags = [
+      {
+        value: "team",
+        count: 3
+      },
+      {
+        value: "project",
+        count: 1
+      },
+      {
+        value: "agility",
+        count: 4
+      },
+      {
+        value: "communication",
+        count: 2
+      },
+      {
+        value: "creativity",
+        count: 2
+      },
+      {
+        value: "autonomy",
+        count: 3
+      },
+      {
+        value: "curiosity",
+        count: 1
+      }
+    ];
+    const customRenderer = tag => {
+      return (
+        <span
+          style={{
+            animation: 'blinker 3s linear infinite',
+            animationDelay: `${Math.random() * 2}s`
+          }}
+          key={tag.value}
+          className={`tag-${tag.count}`}
+          > {t(`skills:soft.skills.${tag.value}`)}</span>);
+    };
+
+    return (
+      <div id="soft">
+        <h4>{t('skills:soft.title')}</h4>
+        <TagCloud tags={tags} minSize={20} maxSize={38} renderer={customRenderer} disableRandomColor shuffle={false}/>
+      </div>
+    );
+  }
+}
+
+Soft.propTypes = {
+  t: PropTypes.func
+};
+
+export default translate(["common", "skills"], {wait: true})(Soft);
