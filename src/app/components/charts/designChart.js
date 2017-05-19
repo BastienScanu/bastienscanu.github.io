94116e6faddd9c3dc2f845772615c68a5ff40df1
Bastien Scanu <bastien42@gmail.com>
2017-05-19 23:48:45 +0200

    Carousel réalisations design

--- src/app/components/charts/designChart.js
+++ src/app/components/charts/designChart.js
@@ -3,9 +3,24 @@ import PropTypes from 'prop-types';
 import {translate} from 'react-i18next';
 
 class DesignChart extends Component {
+  constructor(props) {
+    super(props);
+    this.images = ["sweat", "dynergie", "mineur", "kata"];
+    this.state = {image: 0};
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        image: (this.state.image + 1) % this.images.length
+      });
+    }, 2500);
+  }
+
   render() {
     return (
-      <h3>Design !</h3>
+      <div id="designCarousel" className={this.images[this.state.image]}>
+      </div>
     );
   }
 }
