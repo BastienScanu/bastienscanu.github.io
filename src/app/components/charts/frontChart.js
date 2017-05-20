093e8328a35bf492de5e5e9df8b01ca9b565f00b
Bastien Scanu <bastien42@gmail.com>
2017-05-20 17:16:33 +0200

    animation front-end

--- src/app/components/charts/frontChart.js
+++ src/app/components/charts/frontChart.js
@@ -3,9 +3,31 @@ import PropTypes from 'prop-types';
 import {translate} from 'react-i18next';
 
 class FrontChart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {style: 0};
+    this.nextStyle = this.nextStyle.bind(this);
+    this.intervals = [700, 700, 700, 700, 500, 100, 100, 100, 100, 700, 2000, 5000];
+  }
+
+  componentDidMount() {
+    this.nextStyle();
+  }
+
+  nextStyle() {
+    this.setState({
+      style: (this.state.style + 1) % 12
+    });
+    setTimeout(() => {
+      this.nextStyle();
+    }, this.intervals[this.state.style]);
+  }
+
   render() {
     return (
-      <h3>Front-End !</h3>
+      <div id="frontEnd">
+        <p className={`frontEnd${this.state.style}`}>Front-End !</p>
+      </div>
     );
   }
 }
